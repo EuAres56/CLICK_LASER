684fd6d6436bbe9188aade10479a19bacc241b6f
EuAres56 <mitosobr@gmail.com>
2026-05-18 21:24:34 -0300

    update utils

--- public/script/operation_main.js
+++ public/script/operation_main.js
@@ -900,7 +900,7 @@ REPRINT OS
 */
 function reprintOS(id) {
 
-    const card = document.querySelector(`.os-card .os-id:contains("#${id}")`);
+    const card = document.querySelector(`.os-card .os-card-title .os-id:contains("#${id}")`);
 
     if (!card) {
 
