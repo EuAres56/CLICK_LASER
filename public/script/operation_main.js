1cb869c029a0712dd94e11bfe64e3bd8168f200f
EuAres56 <mitosobr@gmail.com>
2026-05-19 09:25:10 -0300

    ajusts bugs

--- public/script/operation_main.js
+++ public/script/operation_main.js
@@ -855,7 +855,7 @@ async function saveOrder() {
 
         printOrder({
             job_uid: result.uid,
-            date: result.order_created,
+            date: result.created_at,
             client_name: result.client_name,
             product_title: result.product_title,
             text_title: result.text,
